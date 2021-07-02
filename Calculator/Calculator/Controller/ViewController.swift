53d40478c75474cc18857971d39baeba5569d77f
soo941226 <rhrnakrnakr@naver.com>
2021-07-02 17:10:17 +0900

    feat: add IBOutlet of UILabels, add action for editing label.text (ing)

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,12 +8,31 @@ import UIKit
 
 class ViewController: UIViewController {
 	
+    @IBOutlet weak var currentNumberLabel: UILabel!
+    @IBOutlet weak var prevNumberLabel: UILabel!
+    var count = 0
+
+    var currentNumberString = ""{
+        didSet {
+            let value = inputStorage[count]
+            
+            if let valueType = try? CalculatorComponent.convertToComponentType(from: value), valueType == .operator {
+                prevNumberLabel.text? = currentNumberLabel.text ?? "0"
+                currentNumberLabel.text = "0"
+            }
+            
+            self.currentNumberLabel.text? += value
+            count += 1
+        }
+    }
+    
 	private let calculator: Calculatorable = Calculator()
 	private let zero = "0"
-	private var inputStorage: [String] = []
+	private var inputStorage: [String] = ["+"]
 		
 	override func viewDidLoad() {
 		super.viewDidLoad()
+        currentNumberString = "여기엔 아무거나 들어가도 되요 근데 뭐라도 들어가야 해요"
 	}
 	
 	private func updateChangableOperator(with: String) {
@@ -35,6 +54,7 @@ class ViewController: UIViewController {
 	
     @IBAction func touchUpNumberButton(_ sender: UIButton) {
         inputStorage.append("\(sender.tag)")
+        currentNumberString = "숫자가 클릭됨"
     }
     
     @IBAction func touchUpDoubleZeroButton(_ sender: UIButton) {
